2fbeab842f07f86206f78e1a52ab75561e83b6ad
Vaishnavi Pawar <vaishnavi.22211188@viit.ac.in>
2026-09-12 14:31:00 +0530

    create docker login

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
